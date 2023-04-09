9576b97ecade0a1537c9c3d9a35c259b1f26884d
sid <68276495+siddant-rachha@users.noreply.github.com>
2023-04-10 03:15:00 +0530

    added product overlay

--- src/UI/ProductCard/ProductCard.jsx
+++ src/UI/ProductCard/ProductCard.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import classes from './ProductCard.module.css'
 
+//icons
+
+import cart from '../../assets/icons/cart-white.svg'
+import wishlist from '../../assets/icons/wishlist-white.svg'
+import share from '../../assets/icons/share-white.svg'
+
+
 const ProductCard = ({ img, title, price, size = [], priceSlashed }) => {
     return (
         <div className={classes.card}>
             <div className={classes.image}>
                 <img src={img} />
                 <div className={classes.imageOverlay}>
+                    <div><img src={cart}></img></div>
+                    <div><img src={wishlist}></img></div>
+                    <div><img src={share}></img></div>
+
                 </div>
             </div>
             <div className={classes.title}>{title}</div>
